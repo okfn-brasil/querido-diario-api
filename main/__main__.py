1e1f512099a12e007299018fb277d6412629c8f8
José Guilherme Vanz <jvanz@jvanz.com>
2020-09-17 16:29:23 -0300

    Change the database used by the API.
    The API now uses Elastic Search as its backend API. The PostgreSQL is
    not in use anymore.
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

--- main/__main__.py
+++ main/__main__.py
@@ -4,14 +4,13 @@ import uvicorn
 
 from api import app, set_gazette_interface
 from gazettes import create_gazettes_interface
-from database import create_database_data_mapper
+from database import create_elasticsearch_data_mapper
 
-database = os.environ["POSTGRES_DB"]
-user = os.environ["POSTGRES_USER"]
-password = os.environ["POSTGRES_PASSWORD"]
-host = os.environ["POSTGRES_HOST"]
-database_gateway = create_database_data_mapper(database, user, password, host)
-gazettes_interface = create_gazettes_interface(database_gateway)
+host = os.environ["QUERIDO_DIARIO_ELASTICSEARCH_HOST"]
+index = os.environ["QUERIDO_DIARIO_ELASTICSEARCH_INDEX"]
+
+datagateway = create_elasticsearch_data_mapper(host, index)
+gazettes_interface = create_gazettes_interface(datagateway)
 set_gazette_interface(gazettes_interface)
 
 uvicorn.run(app, host="0.0.0.0", port=8080)
