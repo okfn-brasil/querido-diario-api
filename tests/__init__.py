b1bbdf62722eba1d72d298e1c0be0968dec55df6
José Guilherme Vanz <jvanz@jvanz.com>
2020-11-17 15:08:22 -0300

    Remove PostgreSQL data mapper
    The API does not use the PostgreSQL data mapper anymore. This commit removes
    it from the repository.
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

--- tests/__init__.py
+++ tests/__init__.py
@@ -7,12 +7,6 @@ from .gazette_access_tests import (
     GazetteAccessInterfacesTest,
 )
 from .api_tests import ApiGazettesEndpointTests
-from .database_tests import (
-    DatabaseTest,
-    DatabaseInterfacesValidation,
-    DatabaseConnection,
-    DatabaseMigrations,
-)
 
 from .elasticsearch_tests import (
     ElasticSearchInterfaceTest,
