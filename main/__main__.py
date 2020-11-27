b99abdc78ceaa871b43bf5f2ce33fc15a065ee70
José Guilherme Vanz <jvanz@jvanz.com>
2020-11-26 22:57:53 -0300

    Add env var to configure root path
    Adds an environment variable to configure the api to run behind a proxy
    server. Thus, the returned URLs will take the path prefix into account.
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

--- main/__main__.py
+++ main/__main__.py
@@ -8,6 +8,7 @@ from database import create_elasticsearch_data_mapper
 
 host = os.environ["QUERIDO_DIARIO_ELASTICSEARCH_HOST"]
 index = os.environ["QUERIDO_DIARIO_ELASTICSEARCH_INDEX"]
+root_path = os.environ.get("QUERIDO_DIARIO_API_ROOT_PATH", "")
 
 def get_url_prefix():
     return os.environ.get("QUERIDO_DIARIO_URL_PREFIX", "")
@@ -17,4 +18,4 @@ datagateway = create_elasticsearch_data_mapper(host, index)
 gazettes_interface = create_gazettes_interface(datagateway, get_url_prefix())
 set_gazette_interface(gazettes_interface)
 
-uvicorn.run(app, host="0.0.0.0", port=8080)
+uvicorn.run(app, host="0.0.0.0", port=8080, root_path=root_path)
