04c0aa3d85c255acbf8c09594fba82518d5a6a2c
José Guilherme Vanz <jvanz@jvanz.com>
2020-12-01 21:00:24 -0300

    Using config package to load the configuration
    Updates the main package to use the config package to load the app
    configuration and set the values in the objects used by the application
    
    Signed-off-by: José Guilherme Vanz <jvanz@jvanz.com>

--- main/__main__.py
+++ main/__main__.py
@@ -2,20 +2,14 @@ import os
 
 import uvicorn
 
-from api import app, set_gazette_interface
+from api import app, configure_api_app
 from gazettes import create_gazettes_interface
 from database import create_elasticsearch_data_mapper
+from config import load_configuration
 
-host = os.environ["QUERIDO_DIARIO_ELASTICSEARCH_HOST"]
-index = os.environ["QUERIDO_DIARIO_ELASTICSEARCH_INDEX"]
-root_path = os.environ.get("QUERIDO_DIARIO_API_ROOT_PATH", "")
+configuration = load_configuration()
+datagateway = create_elasticsearch_data_mapper(configuration.host, configuration.index)
+gazettes_interface = create_gazettes_interface(datagateway, configuration.url_prefix)
+configure_api_app(gazettes_interface, configuration.root_path)
 
-def get_url_prefix():
-    return os.environ.get("QUERIDO_DIARIO_URL_PREFIX", "")
-
-
-datagateway = create_elasticsearch_data_mapper(host, index)
-gazettes_interface = create_gazettes_interface(datagateway, get_url_prefix())
-set_gazette_interface(gazettes_interface)
-
-uvicorn.run(app, host="0.0.0.0", port=8080, root_path=root_path)
+uvicorn.run(app, host="0.0.0.0", port=8080, root_path=configuration.root_path)
