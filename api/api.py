eb735ee939c175c8d06d8045a5a28ac5e2645b9f
André Formento <andreformento.sc@gmail.com>
2021-06-05 20:46:48 -0300

    ENUM

--- api/api.py
+++ api/api.py
@@ -61,6 +61,13 @@ class CitiesSearchResponse(BaseModel):
     cities: List[City]
 
 
+@unique
+class SortBy(str, Enum):
+    RELEVANCE = "relevance"
+    DESCENDING_DATE = "descending_date"
+    ASCENDING_DATE = "ascending_date"
+
+
 def trigger_gazettes_search(
     territory_id: str = None,
     since: date = None,
@@ -72,7 +79,7 @@ def trigger_gazettes_search(
     number_of_fragments: int = 1,
     pre_tags: List[str] = [""],
     post_tags: List[str] = [""],
-    sort_by: str = "descending_date",
+    sort_by: SortBy = SortBy.DESCENDING_DATE,
 ):
     gazettes_count, gazettes = app.gazettes.get_gazettes(
         GazetteRequest(
@@ -86,7 +93,7 @@ def trigger_gazettes_search(
             number_of_fragments=number_of_fragments,
             pre_tags=pre_tags,
             post_tags=post_tags,
-            sort_by=sort_by,
+            sort_by=sort_by.value,
         )
     )
     response = {
@@ -151,8 +158,8 @@ async def get_gazettes(
         title="Post tags of fragments of highlight.",
         description="Post tags of fragments of highlight. This is a list of strings (usually HTML tags) that will appear after the text which matches the query",
     ),
-    sort_by: Optional[str] = Query(
-        "descending_date",
+    sort_by: Optional[SortBy] = Query(
+        SortBy.DESCENDING_DATE,
         title="Allow the user to define the order of the search results.",
         description="Allow the user to define the order of the search results. The API should allow 3 types: relevance, descending_date, ascending_date",
     ),
@@ -225,8 +232,8 @@ async def get_gazettes_by_territory_id(
         title="Post tags of fragments of highlight.",
         description="Post tags of fragments of highlight. This is a list of strings (usually HTML tags) that will appear after the text which matches the query",
     ),
-    sort_by: Optional[str] = Query(
-        "descending_date",
+    sort_by: Optional[SortBy] = Query(
+        SortBy.DESCENDING_DATE,
         title="Allow the user to define the order of the search results.",
         description="Allow the user to define the order of the search results. The API should allow 3 types: relevance, descending_date, ascending_date",
     ),
