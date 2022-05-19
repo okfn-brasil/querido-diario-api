5097352820eaf6b71edfe58ea354fc90fdf337e6
Giulio Carvalho <giulioccavalcante@gmail.com>
2022-05-19 14:17:51 -0300

    Incrementa versão para v0.13.0

--- api/api.py
+++ api/api.py
@@ -17,7 +17,7 @@ config = load_configuration()
 app = FastAPI(
     title="Querido Diário",
     description="API to access the gazettes from all Brazilian cities",
-    version="0.12.0",
+    version="0.13.0",
 )
 
 app.add_middleware(
