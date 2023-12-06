232e14cb51a5ba84e12543fce63c7d7fb15b5296
Giulio <giulioccavalcante@gmail.com>
2023-12-06 19:00:48 -0300

    Altera versão da API para v0.17.1

--- api/api.py
+++ api/api.py
@@ -20,7 +20,7 @@ config = load_configuration()
 app = FastAPI(
     title="Querido Diário",
     description="API to access the gazettes from all Brazilian cities",
-    version="0.17.0",
+    version="0.17.1",
 )
 
 app.add_middleware(
