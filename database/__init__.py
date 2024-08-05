c850a378ad4b6ffc7db9d035630c0fdc731b4dbd
wildemberg-sales <junior_sales2010@hotmail.com>
2024-08-05 16:20:59 -0300

    Implementa herança de classe para reutilização de código
    Refatora verificação de erros na rota de agregados e Corrige nome de variáveis
    
    Remove try-except da rota de agregados
    
    Remove __init__ das classes do postgres de aggregates e companies
    
    Refatora método de retorno de dados dos agregados
    
    Implementa variavel .env do bucket e retorna com file_path do agregado

--- database/__init__.py
+++ database/__init__.py
@@ -1,13 +1,13 @@
 from companies import CompaniesDatabaseInterface
 from aggregates import AggregatesDatabaseInterface
 
-from .postgresql import PostgreSQLDatabase, PostgreSQLDatabaseAggregates
+from .postgresql import PostgreSQLDatabaseCompanies, PostgreSQLDatabaseAggregates
 
 
 def create_companies_database_interface(
     db_host, db_name, db_user, db_pass, db_port
 ) -> CompaniesDatabaseInterface:
-    return PostgreSQLDatabase(db_host, db_name, db_user, db_pass, db_port)
+    return PostgreSQLDatabaseCompanies(db_host, db_name, db_user, db_pass, db_port)
 
 
 def create_aggregates_database_interface(
